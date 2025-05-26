06e023002787411b6d197474b0e7c245aa5f75ce
ksenov <frostdog080@gmail.com>
2025-05-26 14:20:15 +0300

    gitignore commit

--- core/handlers/main_handlers.py
+++ core/handlers/main_handlers.py
@@ -4,10 +4,13 @@ from aiogram.fsm.context import FSMContext
 from aiogram.types import FSInputFile, CallbackQuery, ContentType
 import random
 
+from config_loader import MESSAGES
+from keyboards.reply.reply_keyboards import start_menu_reply
+
 
 async def get_start(message: Message, state: FSMContext):
     await state.clear()
-    photo = FSInputFile('start_photo.jpg')  # Replace with your photo filename in root folder
+    photo = FSInputFile('media/start_photo.jpg')  # Replace with your photo filename in root folder
     await message.answer_photo(photo, caption=MESSAGES['startup_user'], reply_markup=start_menu_reply())
 
 
